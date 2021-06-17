882085d5298b30814be9793b145fb25105403541
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-18 03:03:26 +0900

    fix for ci

--- packages/core-react/key-maps/index.ts
+++ packages/core-react/key-maps/index.ts
@@ -1,6 +1,8 @@
+import React from "react";
+import { EditorCommand } from "@draft-js-plugins/editor";
 import Draft, { DraftHandleValue } from "draft-js";
 
-export function keyBindingFn(e) {
+export function keyBindingFn(e: React.KeyboardEvent<Element>) {
   if (e.key === "/") {
     return "show-boring-shortcut-menu";
   }
@@ -9,7 +11,7 @@ export function keyBindingFn(e) {
   return Draft.getDefaultKeyBinding(e);
 }
 
-export function handleKeyCommand(command): DraftHandleValue {
+export function handleKeyCommand(command: EditorCommand): DraftHandleValue {
   if (command === "show-boring-shortcut-menu") {
     // todo
     return "handled";

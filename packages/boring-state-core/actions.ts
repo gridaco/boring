a20f14f3a5687fe5411dc02d4b2738dbe9dbc58c
UZU, J <woojoo@softmarshmallow.com>
2021-06-25 14:22:14 +0900

    add boring default actions definitions

---
+++ packages/boring-state-core/actions.ts
@@ -0,0 +1,70 @@
+type BlockCreationActionType =
+  | typeof add_any_block
+  //
+  | typeof add_code_block
+  | HeadingBlockCreationActionType
+  | typeof add_paragraph_block;
+
+interface IAddBlockAction {
+  type: BlockCreationActionType;
+}
+
+const add_any_block = "add-any-block";
+interface AddAnyBlockAction extends IAddBlockAction {
+  type: typeof add_any_block;
+}
+
+const add_code_block = "add-code-block";
+interface AddCodeBlock extends IAddBlockAction {
+  type: typeof add_code_block;
+}
+
+type HeadingBlockCreationActionType =
+  | typeof add_heading_block
+  | typeof add_heading1_block
+  | typeof add_heading2_block
+  | typeof add_heading3_block
+  | typeof add_heading4_block
+  | typeof add_heading5_block
+  | typeof add_heading6_block;
+
+const add_heading_block = "add-heading-block";
+const add_heading1_block = "add-heading1-block";
+const add_heading2_block = "add-heading2-block";
+const add_heading3_block = "add-heading3-block";
+const add_heading4_block = "add-heading4-block";
+const add_heading5_block = "add-heading5-block";
+const add_heading6_block = "add-heading6-block";
+
+interface IAddHeadingBlockAction extends IAddBlockAction {
+  type: HeadingBlockCreationActionType;
+}
+interface AddHeadingBlock extends IAddHeadingBlockAction {
+  type: typeof add_heading_block;
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+}
+interface AddHeading1Block extends IAddHeadingBlockAction {
+  type: typeof add_heading1_block;
+}
+interface AddHeading2Block extends IAddHeadingBlockAction {
+  type: typeof add_heading2_block;
+}
+interface AddHeading3Block extends IAddHeadingBlockAction {
+  type: typeof add_heading3_block;
+}
+interface AddHeading4Block extends IAddHeadingBlockAction {
+  type: typeof add_heading4_block;
+}
+interface AddHeading5Block extends IAddHeadingBlockAction {
+  type: typeof add_heading5_block;
+}
+interface AddHeading6Block extends IAddHeadingBlockAction {
+  type: typeof add_heading6_block;
+}
+
+//
+
+const add_paragraph_block = "add-paragraph-block";
+interface AddParagraphBlock extends IAddBlockAction {
+  type: typeof add_paragraph_block;
+}
